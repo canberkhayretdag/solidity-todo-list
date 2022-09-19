b2f01871013a92f08b19a51ae6897b3f97bc3482
Canberk Hayretdağ <39572277+canberkhayretdag@users.noreply.github.com>
2022-09-19 18:55:27 +0300

    Update TodoList.sol

--- TodoList.sol
+++ TodoList.sol
@@ -26,14 +26,11 @@ contract Todos {
         todos.push(todo);
     }
 
-    // Solidity automatically created a getter for 'todos' so
-    // you don't actually need this function.
     function get(uint _index) public view returns (string memory text, bool completed) {
         Todo storage todo = todos[_index];
         return (todo.text, todo.completed);
     }
 
-    // update text
     function updateText(uint _index, string calldata _text) public {
         Todo storage todo = todos[_index];
         todo.text = _text;
